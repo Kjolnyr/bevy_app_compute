e2ed4b5279f9a450eb643aaab53eab26c57c9cfd
Anthony Tornetta <cornchipgonecodin@gmail.com>
2024-07-25 13:11:33 -0400

    Created system sets for end-users to properly order their systems

--- src/plugin.rs
+++ src/plugin.rs
@@ -16,10 +16,32 @@ impl Plugin for AppComputePlugin {
     fn finish(&self, app: &mut App) {
         let render_device = app.world().resource::<RenderDevice>().clone();
 
+        app.configure_sets(Update, BevyEasyComputeSet::ExtractPipelines)
+            .configure_sets(PostUpdate, BevyEasyComputePostUpdateSet::ExecuteCompute);
+
         app.insert_resource(AppPipelineCache::new(render_device))
             .add_systems(PreUpdate, extract_shaders)
-            .add_systems(Update, process_pipeline_queue_system);
+            .add_systems(
+                Update,
+                process_pipeline_queue_system.in_set(BevyEasyComputeSet::ExtractPipelines),
+            );
     }
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, SystemSet)]
+/// Extracts data from GPU and enables it to be read
+pub enum BevyEasyComputeSet {
+    /// Extracts data from GPU and enables it to be read
+    ExtractPipelines,
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, SystemSet)]
+/// Sends needed data to the GPU and runs the compute shader
+pub enum BevyEasyComputePostUpdateSet {
+    /// Sends needed data to the GPU and runs the compute shader
+    ///
+    /// Shaders will also be checked for completion in this set
+    ExecuteCompute,
 }
 
 /// Plugin to initialise your [`AppComputeWorker<W>`] structs.
@@ -42,10 +64,17 @@ impl<W: ComputeWorker> Plugin for AppComputeWorkerPlugin<W> {
         let worker = W::build(app.world_mut());
 
         app.insert_resource(worker)
-            .add_systems(Update, AppComputeWorker::<W>::extract_pipelines)
+            .add_systems(
+                Update,
+                AppComputeWorker::<W>::extract_pipelines
+                    .in_set(BevyEasyComputeSet::ExtractPipelines)
+                    .after(process_pipeline_queue_system),
+            )
             .add_systems(
                 PostUpdate,
-                (AppComputeWorker::<W>::unmap_all, AppComputeWorker::<W>::run).chain(),
+                (AppComputeWorker::<W>::unmap_all, AppComputeWorker::<W>::run)
+                    .in_set(BevyEasyComputePostUpdateSet::ExecuteCompute)
+                    .chain(),
             );
     }
 }
